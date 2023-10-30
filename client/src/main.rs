457a0331ee244ca0565260e325a5b87eb787011f
wangcham <651122857@qq.com>
2023-10-30 20:02:43 +0800

    完成基本功能

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,18 +1,58 @@
-use std::io::{self, Write};
+use std::env;
+use std::fs;
+use std::io::{self,Write};
+use std::net::TcpStream;
+use std::process;
+use std::path::Path;
+use std::string;
 
-fn main() {
-    loop {
-        print!("请输入命令: ");
+fn main(){
+    println!("请直接输入文件路径或输入quit退出:");
+    loop{
+        print!(">>>");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
+        let input = input.trim();
         match input.trim() {
-            "hello" => println!("你好！"),
-            "quit" => break,
-            _ => println!("未知命令"),
+            "RIP" => print!("Matthew Perry\n"),
+            "q" => break,
+             _ => check(&input),
+        }
+    }
+
+}
+// check if the file exists
+fn check(file_path:&str){
+    let path = Path::new(file_path);
+    if path.exists(){
+        print!("存在此文件\n");
+        enterinfo(file_path);
+    }else{
+        print!("文件不存在\n");
+    }
+}
+
+fn enterinfo(file_path: &str) {
+    print!("请输入ip地址和端口号:\n");
+    let mut addr = String::new();
+    io::stdin().read_line(&mut addr).unwrap();
+    let addr = addr.trim();
+
+    // 读取文件内容
+    let contents = fs::read(file_path).expect("无法读取文件");
+
+    match TcpStream::connect(addr) {
+        Ok(mut stream) => {
+            stream.write_all(&contents)
+                .expect("无法发送文件");
+            println!("文件已发送至服务器");
+        }
+        Err(e) => {
+            eprintln!("无法连接到服务器: {}", e);
         }
     }
 }
+
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512]; // 创建一个缓冲区来存储接收到的数据
+    let mut file = File::create("received_file").unwrap(); // 创建一个文件来保存接收到的数据
+
+    loop {
+        let nbytes = stream.read(&mut buffer).unwrap(); // 从流中读取数据
+        if nbytes == 0 {
+            break; // 如果没有读取到数据，那么就跳出循环
+        }
+        file.write_all(&buffer[..nbytes]).unwrap(); // 将数据写入文件
+    }
+
+    println!("文件已接收并保存到本地");
 }
+
+fn main() {
+    let listener = TcpListener::bind("localhost:12345").unwrap(); // 在指定的地址和端口上创建一个监听器
+
+    for stream in listener.incoming() { // 对于每一个连接到服务器的客户端
+        match stream {
+            Ok(stream) => { // 如果连接成功
+                thread::spawn(|| { // 创建一个新的线程来处理客户端
+                    handle_client(stream); // 处理客户端
+                });
+            }
+            Err(e) => { // 如果连接失败
+                eprintln!("无法连接到客户端: {}", e);
+            }
+        }
+    }
+}
+
